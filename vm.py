285599ca10d1a70441999fabdc62d248affc424c
xunoaib <xunoaib@gmail.com>
2023-01-07 22:20:01 -0500

    Fix reading user input

--- vm.py
+++ vm.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import readline
 import sys
 from colorama import Fore, Style
 from specs import OPCODES, WORD_BYTES, load_bytecode
@@ -16,14 +17,15 @@ class CPU:
         self.memory = {}
         self.registers = [0] * 8
         self.stack = []
+        self.input_buffer = []
 
     def readvalue(self, arg):
-        if arg >= 32768:
+        if isreg(arg):
             return self.registers[arg - 32768]
         return arg
 
     def val_to_reg(self, arg):
-        if arg >= 32768:
+        if isreg(arg):
             return arg - 32768
         return arg
 
@@ -39,7 +41,6 @@ class CPU:
 
         pc = 0
         while True:
-            # print(bytecode[pc-5:pc+10])
             opcode, args, nextpc = self.read_instruction(bytecode, pc)
             debug(pc, opcode.name, args)
             a, b, c = args + (None,) * (3 - len(args))
@@ -47,68 +48,85 @@ class CPU:
             match opcode.name:
                 case 'noop':
                     pass
+
+                case 'halt':
+                    break
+
                 case 'out':
                     a = self.readvalue(a)
                     print(chr(a), end='')
-                case 'halt':
-                    break
+
                 case 'jmp':
                     nextpc = a
+
                 case 'jt':
                     a = self.readvalue(a)
                     if a != 0:
                         nextpc = b
+
                 case 'jf':
                     a = self.readvalue(a)
                     if a == 0:
                         nextpc = b
+
                 case 'set':
                     a = self.val_to_reg(a)
                     b = self.readvalue(b)
                     self.registers[a] = b
+
                 case 'add':
                     a = self.val_to_reg(a)
                     b = self.readvalue(b)
                     c = self.readvalue(c)
                     self.registers[a] = (b + c) % 32768
+
                 case 'eq':
                     a = self.val_to_reg(a)
                     b = self.readvalue(b)
                     c = self.readvalue(c)
                     self.registers[a] = int(b == c)
+
                 case 'push':
                     a = self.readvalue(a)
                     self.stack.append(a)
+
                 case 'pop':
                     a = self.val_to_reg(a)
                     self.registers[a] = self.stack.pop()
+
                 case 'gt':
                     a = self.val_to_reg(a)
                     b = self.readvalue(b)
                     c = self.readvalue(c)
                     self.registers[a] = int(b > c)
+
                 case 'and':
                     a = self.val_to_reg(a)
                     b = self.readvalue(b)
                     c = self.readvalue(c)
                     self.registers[a] = int(b & c)
+
                 case 'or':
                     a = self.val_to_reg(a)
                     b = self.readvalue(b)
                     c = self.readvalue(c)
                     self.registers[a] = int(b | c)
+
                 case 'not':
                     a = self.val_to_reg(a)
                     b = self.readvalue(b)
                     self.registers[a] = ~b & (2**15-1)
+
                 case 'call':
                     self.stack.append(nextpc)
                     nextpc = self.readvalue(a)
+
                 case 'mult':
                     a = self.val_to_reg(a)
                     b = self.readvalue(b)
                     c = self.readvalue(c)
                     self.registers[a] = (b * c) % 32768
+
                 case 'mod':
                     a = self.val_to_reg(a)
                     b = self.readvalue(b)
@@ -138,17 +156,14 @@ class CPU:
                 # encountered; this means that you can safely read whole lines from the
                 # keyboard and trust that they will be fully read
                 case 'in':
-                    # print()
-                    # print(self.stack)
-                    # print(self.registers)
-                    # print()
-
                     a = self.val_to_reg(a)
-                    if command := input('> ').strip():
-                        self.registers[a] = ord(command[0])
-                    else:
-                        print('Invalid')
-                        nextpc = pc
+                    if not self.input_buffer:
+                        cmd = list(input('> ') + '\n')[::-1]
+                        if not cmd:
+                            print('Invalid')
+                            continue
+                        self.input_buffer = cmd
+                    self.registers[a] = ord(self.input_buffer.pop())
 
                 case _:
                     print('unimplemented:', opcode, args)
