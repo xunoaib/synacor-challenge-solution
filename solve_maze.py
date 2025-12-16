d9e54d4ea4a337e17b121cf455bd6e81a38a0675
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-12-16 15:02:36 -0500

    Identify items while exploring locations

--- solve_maze.py
+++ solve_maze.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
+import json
 import pickle
 import re
 from heapq import heappop, heappush
 
+import utils
 from enhancedcpu import EnhancedCPU
 
 
-def next_states(vm: EnhancedCPU):
-    exits = get_exits(vm)
+def neighbor_locs(vm: EnhancedCPU) -> list[tuple[str, EnhancedCPU]]:
+    exits = find_exits(vm)
     vms = []
     for direction in exits:
         n = vm.sendcopy(direction)
@@ -15,36 +17,69 @@ def next_states(vm: EnhancedCPU):
     return vms
 
 
-def get_exits(vm: EnhancedCPU):
+def neighbor_items(vm: EnhancedCPU):
+    items = find_items(vm)
+    vms = []
+    for item in items:
+        cmd = 'take ' + item
+        n = vm.sendcopy(cmd)
+        vms.append((cmd, n))
+    return vms
+
+
+def all_neighbors(vm: EnhancedCPU):
+    return neighbor_items(vm) + neighbor_locs(vm)
+
+
+def find_items(vm: EnhancedCPU):
     vm.read()
     vm = vm.sendcopy('look')
     data = vm.read()
-    if exitsstr := re.search(
-        r'\nThere (is|are) (\d+) exits?:\n(.*)\nWhat do you do?', data,
-        re.DOTALL
+
+    if m := re.search(
+        r'\nThings of interest here:\n(.*?)\n\n', data, re.DOTALL
     ):
-        return re.findall(r'- (.*?)\n', exitsstr.group(3))
+        return re.findall(r'- (.*?)\n', m.group(1) + '\n')
+
     return []
 
 
-def explore(current: EnhancedCPU):
+def find_exits(vm: EnhancedCPU):
+    vm.read()
+    vm = vm.sendcopy('look')
+    data = vm.read()
+
+    if m := re.search(
+        r'\nThere (is|are) (\d+) exits?:\n(.*)\nWhat do you do?', data,
+        re.DOTALL
+    ):
+        return re.findall(r'- (.*?)\n', m.group(3))
+    return []
+
+
+def explore(current: EnhancedCPU, verbose=True):
     '''Explore all possible paths from the given CPU state. Cycles are skipped'''
 
     current.read()
     current.send('look')
 
-    vms = {}
-    edges = {}  # maps current vm location => [(north_room_id, 'north'), ...]
+    vms: dict[int, EnhancedCPU] = {}
+    # map current vm location => [(north_room_id, 'north'), ...]
+    edges: dict[int, list[tuple[int, str]]] = {}
     descriptions = {current.location: current.read().strip()}
 
     q = [current]
 
     while q:
         current = q.pop(0)
-        print(current.location, descriptions[current.location].split('\n')[0])
-        print(descriptions[current.location])
+        if verbose:
+            print(
+                f'\033[92m{current.location}',
+                descriptions[current.location].split('\n')[0], '\033[0m'
+            )
+            print(descriptions[current.location])
 
-        states = list(next_states(current))
+        states = list(neighbor_locs(current))
         edges[current.location] = [
             (neighbor.location, next_move) for next_move, neighbor in states
         ]
@@ -68,11 +103,28 @@ def explore(current: EnhancedCPU):
 
 def main():
     # vm = EnhancedCPU('challenge.bin')
-    # vm = EnhancedCPU.from_snapshot_file('snapshots/start')
+    vm = EnhancedCPU.from_snapshot_file('snapshots/start')
     # vm.send('.giveall')
     # vm = EnhancedCPU.from_snapshot_file('snapshots/ladder')
-    vm = EnhancedCPU.from_snapshot_file('snapshots/beach')
-    edges, descs, vms = explore(vm)
+    # vm = EnhancedCPU.from_snapshot_file('snapshots/beach')
+
+    # Find all states (without picking up items)
+    edges, descs, vms = explore(vm, verbose=False)
+
+    # Find states with items
+    item_addrs = []
+    for loc, vm in vms.items():
+        if items := find_items(vm):
+            for item in items:
+                vm2 = vm.sendcopy('take ' + item)
+                result = utils.diff_vms(vm, vm2)
+                # print(f'took {item} => {result}')
+                addr = next(
+                    idx for idx, old, new in result.get('memory', [])
+                    if old and not new
+                )
+                print(f'took {item} => {addr}')
+
     return
 
     # # discover all rooms (and cache to file)
