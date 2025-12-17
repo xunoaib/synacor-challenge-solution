d2d730e80f3377098ffc0e58c1b2e7e9c734bf01
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-12-17 03:28:19 -0500

    Update location identifier

--- identify_location.py
+++ identify_location.py
@@ -6,59 +6,33 @@ from enhancedcpu import EnhancedCPU
 fname = 'challenge-aneurysm.bin'
 
 
-def identify_location_addr():
-    vm = EnhancedCPU(fname)
+def identify_location_addr(vm: EnhancedCPU):
     vm.run()
     vms = [vm]
 
     for cmd in ['doorway', 'north', 'north']:
         vms.append(vms[-1].sendcopy(cmd))
 
+    loc_addr = None
     for a, b in pairwise(vms):
         mem = utils.diff_vms(a, b)['memory']
         addrs = [d[0] for d in mem]
-        # print(mem)
-        print(addrs)
-
         loc_addr = addrs[0]  # assume lowest (may be incorrect)
 
-    print(f'{loc_addr=}')
-    # vm.interactive()
-
-
-def identify_tele_call():
-
-    # Code to search for (ignoring special memory addresses)
-    code = [
-        7, 32768, None, 9, 32768, 32769, 1, 18, 7, 32769, None, 9, 32768,
-        32768, 32767, 1, 32769, 32775, 17, None, 18, 2, 32768, 9, 32769, 32769,
-        32767, 17, None, 1, 32769, 32768, 3, 32768, 9, 32768, 32768, 32767, 17,
-        None, 18
-    ]
-
-    v1 = EnhancedCPU(fname)
-    v1.run()
-    print(v1.memory[6049:6090])
-
-    v2 = EnhancedCPU('challenge.bin')
-    v2.run()
-    print(v2.memory[6027:6027 + (6090 - 6049)])
-
-    print(find_code(v1.memory, code))
-    print(find_code(v2.memory, code))
-
-
-def find_code(memory: list[int], code: list[int | None]):
-    n = len(code)
-    return [
-        i for i in range(len(memory))
-        if all(m == c for m, c in zip(memory[i:i + n], code) if c is not None)
-    ]
+    assert loc_addr
+    return loc_addr
 
 
 def main():
-    # identify_location_addr()
-    identify_tele_call()
+
+    vm = EnhancedCPU(fname)
+    addr = identify_location_addr(vm)
+    print(addr)
+
+    # v2 = EnhancedCPU('challenge.bin')
+    # v2.run()
+    # addr = utils.find_teleporter_call(v2.memory)
+    # print(addr)
 
 
 if __name__ == '__main__':
