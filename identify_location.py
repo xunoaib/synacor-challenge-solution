ecec70e594e0e26b5c8986054a56efbe8ee9080e
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-12-17 00:45:58 -0500

    Search for unoptimized teleporter call

--- identify_location.py
+++ identify_location.py
@@ -3,9 +3,11 @@ from itertools import pairwise
 import utils
 from enhancedcpu import EnhancedCPU
 
+fname = 'challenge-aneurysm.bin'
 
-def main():
-    vm = EnhancedCPU('challenge-aneurysm.bin')
+
+def identify_location_addr():
+    vm = EnhancedCPU(fname)
     vm.run()
     vms = [vm]
 
@@ -21,8 +23,42 @@ def main():
         loc_addr = addrs[0]  # assume lowest (may be incorrect)
 
     print(f'{loc_addr=}')
-
     # vm.interactive()
+
+
+def identify_tele_call():
+
+    # Code to search for (ignoring special memory addresses)
+    code = [
+        7, 32768, None, 9, 32768, 32769, 1, 18, 7, 32769, None, 9, 32768,
+        32768, 32767, 1, 32769, 32775, 17, None, 18, 2, 32768, 9, 32769, 32769,
+        32767, 17, None, 1, 32769, 32768, 3, 32768, 9, 32768, 32768, 32767, 17,
+        None, 18
+    ]
+
+    v1 = EnhancedCPU(fname)
+    v1.run()
+    print(v1.memory[6049:6090])
+
+    v2 = EnhancedCPU('challenge.bin')
+    v2.run()
+    print(v2.memory[6027:6027 + (6090 - 6049)])
+
+    print(find_code(v1.memory, code))
+    print(find_code(v2.memory, code))
+
+
+def find_code(memory: list[int], code: list[int | None]):
+    n = len(code)
+    return [
+        i for i in range(len(memory))
+        if all(m == c for m, c in zip(memory[i:i + n], code) if c is not None)
+    ]
+
+
+def main():
+    # identify_location_addr()
+    identify_tele_call()
 
 
 if __name__ == '__main__':
