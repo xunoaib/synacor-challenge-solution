a7e87dad81e612744ef7b94cdffcb6f3dc485d08
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-12-19 00:16:13 -0500

    Clean up strings.py

--- strings.py
+++ strings.py
@@ -3,25 +3,30 @@ import string
 from vm import VM
 
 
-def strings(memory: list[int], min_len: int):
-    groups = {}
+def strings(memory: list[int]):
+    addr_strs = {}
     laddr = 0
 
     for addr, v in enumerate(memory):
         if v < 256 and chr(v) in string.printable:
-            groups[laddr] = groups.get(laddr, '') + chr(v)
+            addr_strs[laddr] = addr_strs.get(laddr, '') + chr(v)
         else:
             laddr = addr + 1
 
-    for a, s in groups.items():
-        if len(s) >= min_len:
-            print(a, repr(s))
+    return addr_strs
 
 
 def main():
-    vm = VM('challeng.bin')
+    vm = VM('challenge.bin')
     vm.run()
-    strings(vm.memory, 2)
+
+    addr_strs = strings(vm.memory)
+
+    MIN_STR_LEN = 2
+
+    for addr, s in addr_strs.items():
+        if len(s) >= MIN_STR_LEN:
+            print(addr, repr(s))
 
 
 if __name__ == '__main__':
