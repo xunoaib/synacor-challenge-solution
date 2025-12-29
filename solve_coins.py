cced780178394bcdbd825dbc29b6581da0120413
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-12-29 02:47:23 -0500

    Update solve_coins.py

--- solve_coins.py
+++ solve_coins.py
@@ -1,6 +1,6 @@
 from itertools import combinations, permutations
 
-from z3 import Int, Or, Solver
+from z3 import Int, Or, Solver, sat
 
 COIN_NAMES = {
     2: 'red coin',
@@ -10,11 +10,9 @@ COIN_NAMES = {
     9: 'blue coin',
 }
 
-COIN_VALS = [2, 3, 5, 7, 9]
-
 
 def solve_coin_order():
-    for a, b, c, d, e in permutations(COIN_VALS):
+    for a, b, c, d, e in permutations(COIN_NAMES):
         if a + b * c**2 + d**3 - e == 399:
             return [a, b, c, d, e]
 
@@ -24,27 +22,18 @@ def solve_coin_order_z3():
 
     s = Solver()
     s.add(a + b * c**2 + d**3 - e == 399)
+    s.add(x != y for x, y in combinations(vals, 2))
+    s.add(Or([v == n for n in COIN_NAMES]) for v in vals)
 
-    for x, y in combinations(vals, 2):
-        s.add(x != y)
-
-    for v in vals:
-        s.add(Or([v == n for n in COIN_VALS]))
-
+    assert s.check() == sat
     m = s.model()
-    return [m[v] for v in vals]
+    return [m[v].as_long() for v in vals]
 
 
-def main():
-    coins = solve_coin_order()
-    print('coin order:', coins, '\n')
+coins = solve_coin_order()
+# coins = solve_coin_order_z3()
 
-    for coin in coins:
-        print('use', COIN_NAMES[coin])
+print('coin order:', coins, '\n')
 
-
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
+for coin in coins:
+    print('use', COIN_NAMES[coin])
